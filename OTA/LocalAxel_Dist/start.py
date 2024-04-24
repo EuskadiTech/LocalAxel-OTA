89481a3852f3b3fd3f9f1f604bb02540b925122c
Nulldev <109038805+naielv@users.noreply.github.com>
2024-04-25 00:17:14 +0200

    Update

--- OTA/LocalAxel_Dist/start.py
+++ OTA/LocalAxel_Dist/start.py
@@ -2,4 +2,17 @@ import utils
 
 if __name__ == "__main__":
     utils.ota_updater()
+    print("\n" * 6)
+    print("==============================")
+    print("   LocalAxel se ha iniciado   ")
+    print("==============================")
+    print("                              ")
+    print("  Abre la siguiente url para  ")
+    print("            entrar            ")
+    print("                              ")
+    print("                              ")
+    print(" http://127.0.0.1:17170/      ")
+    print("==============================")
+    print("                              ")
+
     __import__("main").app.run(host="0.0.0.0", port=17170, debug=False)
