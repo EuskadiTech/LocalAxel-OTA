f77192dd4eab704bb3588b4b1987b8b17663dcef
Nulldev <109038805+naielv@users.noreply.github.com>
2024-04-25 00:09:34 +0200

    Update config.py

--- OTA/LocalAxel_Dist/config.py
+++ OTA/LocalAxel_Dist/config.py
@@ -4,8 +4,9 @@ LANG = "ES"
 
 ### Ajustes Actualizaciones (OTA)
 # Si quieres desactivar los cambios, NO TOQUES ESTO, Cambia OTA_Disable
+# Solo hazlo una vez instalado
 OTA_URL_Prefix = f"https://raw.githubusercontent.com/EuskadiTech/LocalAxel-OTA/main/OTA/LocalAxel/{LANG}/{{}}"
-OTA_Disable = True
+OTA_Disable = False
 
 ### Menu Comedor
 Menu_URL = "https://raw.githubusercontent.com/EuskadiTech/LocalAxel-OTA/main/OTA/Comedor/test.json"
