bc564305fb5e3574840a7389f46e31dac818ed7a
Nulldev <109038805+naielv@users.noreply.github.com>
2024-04-25 00:08:59 +0200

    Update start.py

--- OTA/LocalAxel_Dist/start.py
+++ OTA/LocalAxel_Dist/start.py
@@ -1,6 +1,5 @@
-import main
 import utils
 
 if __name__ == "__main__":
     utils.ota_updater()
-    main.app.run(host="0.0.0.0", port=17170, debug=False)
+    __import__("main").app.run(host="0.0.0.0", port=17170, debug=False)
